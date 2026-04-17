62f9980a20e4b7b6a730aafc2d9138afdfd5fced
Danielle Anderson <danderson1@unm.edu>
2026-04-16 18:30:21 -0600

    initial API

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
+// Form submission endpoint
+app.post('/submit-form', (req, res) => {
+  console.log('📩 Form received:', req.body);
+
+  res.json({
+    message: 'Form received successfully',
+    data: req.body
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
